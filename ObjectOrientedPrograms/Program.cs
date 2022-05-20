4598a0fd8c1010562eeb68985e553a4d77e42dc4
Anant1202 <anantsingh1202@gmail.com>
2022-05-20 16:06:36 +0530

    Welcome to Object Oriented Programs

---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ObjectOrientedPrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Object Oriented Programs");
+        }
+    }
+}
+
